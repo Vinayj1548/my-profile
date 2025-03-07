5d4cc1de63b4007e2894f3a9fa33791634f5dd2e
Vinayj1548 <vjdevil22914125@gmail.com>
2025-03-08 00:20:55 +0530

    fixed-ui

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,13 +4,12 @@ import About from "@/components/sections/About";
 import Skills from "@/components/sections/Skills";
 import Projects from "@/components/sections/Projects";
 import Contact from "@/components/sections/Contact";
-import D3Background from "../components/sections/D3background";
+
 
 
 export default function Home() {
   return (
     <>
-      <D3Background/>
     <div className="min-h-screen bg-gray-900 text-white">
       <Navbar />
       <main className="max-w-screen-xl mx-auto px-6 sm:px-12 flex flex-col gap-12 sm:gap-16">
